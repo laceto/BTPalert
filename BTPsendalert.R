459e9eb045e551abb1d1dcb78f71239e4ec9de29
Luigi Aceto <Luigi.Aceto@genre.com>
2022-10-21 11:15:48 +0200

    try

--- BTPsendalert.R
+++ BTPsendalert.R
@@ -1,12 +1,20 @@
 library(tidyverse)
 library(glue)
 library(rvest)
+library(gmailr)
 ISIN <- "IT0003934657"
 
 pricenow <- scrapepriceBTP(ISIN = ISIN)
 date <- Sys.time()
 row <- data.frame(date, pricenow)
 write_csv(row,paste0('data/pricenow.csv'),append = T)
+
+gm_auth_configure(path = "BTPalert.json")
+GMAILR_APP <- "BTPalert.json"
+# usethis::edit_r_environ()
+# gm_auth_configure()
+gm_auth(email = "luigi.vinegar@gmail.com")
+
 
 targetprice <- 93
 
